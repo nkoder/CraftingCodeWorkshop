dd8aeadc0b8beffb1a05d25d8e80918e3bf86332
Pawel Barszcz <pawelbarszcz@gmail.com>
2015-01-29 13:16:23 +0100

    Exercise 1: JUnitParams in tests.

--- exercise1/src/test/java/pl/nkoder/craftingcodeworkshop/exercise1/RomanNumeralConverterShould.java
+++ exercise1/src/test/java/pl/nkoder/craftingcodeworkshop/exercise1/RomanNumeralConverterShould.java
@@ -1,37 +1,56 @@
 package pl.nkoder.craftingcodeworkshop.exercise1;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static junitparams.JUnitParamsRunner.$;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class RomanNumeralConverterShould {
 
+    @Parameters(method = "decimalsAndRomans")
     @Test
-    public void generate_a_roman_numeral_for_a_given_decimal() {
-        assertThat(romanFor(1)).isEqualTo("I");
-        assertThat(romanFor(2)).isEqualTo("II");
-        assertThat(romanFor(3)).isEqualTo("III");
-        assertThat(romanFor(4)).isEqualTo("IV");
-        assertThat(romanFor(5)).isEqualTo("V");
-        assertThat(romanFor(7)).isEqualTo("VII");
-        assertThat(romanFor(9)).isEqualTo("IX");
-        assertThat(romanFor(10)).isEqualTo("X");
-        assertThat(romanFor(17)).isEqualTo("XVII");
-        assertThat(romanFor(30)).isEqualTo("XXX");
-        assertThat(romanFor(38)).isEqualTo("XXXVIII");
-        assertThat(romanFor(40)).isEqualTo("XL");
-        assertThat(romanFor(90)).isEqualTo("XC");
-        assertThat(romanFor(50)).isEqualTo("L");
-        assertThat(romanFor(90)).isEqualTo("XC");
-        assertThat(romanFor(100)).isEqualTo("C");
-        assertThat(romanFor(400)).isEqualTo("CD");
-        assertThat(romanFor(500)).isEqualTo("D");
-        assertThat(romanFor(900)).isEqualTo("CM");
-        assertThat(romanFor(1000)).isEqualTo("M");
-        assertThat(romanFor(3999)).isEqualTo("MMMCMXCIX");
+    public void generate_a_roman_numeral_for_a_given_decimal(int decimal, String roman) {
+        assertThat(romanFor(decimal)).isEqualTo(roman);
+    }
+
+    @Test
+    public void generate_a_roman_numeral_for_tricky_decimal() {
+        assertThat(romanFor(444)).isEqualTo("CDXLIV");
+        assertThat(romanFor(999)).isEqualTo("CMXCIX");
     }
 
     private String romanFor(int decimal) {
         return new RomanNumeralConverter().convert(decimal);
+    }
+
+    @SuppressWarnings("UnusedDeclaration")
+    private Object decimalsAndRomans() {
+        return $(
+                $(1, "I"),
+                $(2, "II"),
+                $(3, "III"),
+                $(4, "IV"),
+                $(5, "V"),
+                $(7, "VII"),
+                $(9, "IX"),
+                $(10, "X"),
+                $(17, "XVII"),
+                $(30, "XXX"),
+                $(38, "XXXVIII"),
+                $(40, "XL"),
+                $(90, "XC"),
+                $(50, "L"),
+                $(90, "XC"),
+                $(100, "C"),
+                $(400, "CD"),
+                $(500, "D"),
+                $(900, "CM"),
+                $(1000, "M"),
+                $(3999, "MMMCMXCIX")
+        );
     }
 }
