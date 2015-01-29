159e0caab3f58bcb0e4fe8120c4a7cb119d9814d
Pawel Barszcz <pawelbarszcz@gmail.com>
2015-01-30 00:06:54 +0100

    Exercise 2 - Account: implemented SystemClock.

--- exercise2/src/main/java/pl/nkoder/craftingcodeworkshop/exercise2/SystemClock.java
+++ exercise2/src/main/java/pl/nkoder/craftingcodeworkshop/exercise2/SystemClock.java
@@ -2,9 +2,11 @@ package pl.nkoder.craftingcodeworkshop.exercise2;
 
 import java.time.LocalDate;
 
+import static java.time.LocalDate.now;
+
 public class SystemClock {
 
     public LocalDate currentDate() {
-        return null;
+        return now();
     }
 }
--- exercise2/src/main/java/pl/nkoder/craftingcodeworkshop/exercise2/Transactions.java
+++ exercise2/src/main/java/pl/nkoder/craftingcodeworkshop/exercise2/Transactions.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 public class Transactions {
+
     public void storeTransaction(int amount, LocalDate date) {
 
     }
