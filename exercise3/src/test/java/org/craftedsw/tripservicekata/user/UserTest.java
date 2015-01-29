d5db5a668b006cc35acb1d637f8008c9facb7f2e
Pawel Barszcz <pawelbarszcz@gmail.com>
2015-01-29 15:41:48 +0100

    Exercise 3: init

---
+++ exercise3/src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -0,0 +1,15 @@
+package org.craftedsw.tripservicekata.user;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserTest {
+
+    @Test
+    public void name() {
+        assertThat(true).isFalse();
+
+    }
+}
