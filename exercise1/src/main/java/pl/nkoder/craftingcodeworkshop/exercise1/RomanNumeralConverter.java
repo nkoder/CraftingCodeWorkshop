171a39b975f415ee763b53f6aafc3e0d5a93b8d7
Pawel Barszcz <pawelbarszcz@gmail.com>
2015-01-29 11:43:22 +0100

    Exercise 1 - Roman Numerals: converting 1s, 5s, 10s, and their repetitions.

--- exercise1/src/main/java/pl/nkoder/craftingcodeworkshop/exercise1/RomanNumeralConverter.java
+++ exercise1/src/main/java/pl/nkoder/craftingcodeworkshop/exercise1/RomanNumeralConverter.java
@@ -3,6 +3,28 @@ package pl.nkoder.craftingcodeworkshop.exercise1;
 public class RomanNumeralConverter {
 
     public String convert(int decimal) {
-        return "I";
+        String roman = "";
+        for (RomanToDecimal romanToDecimal : RomanToDecimal.values()) {
+            while (decimal >= romanToDecimal.decimal) {
+                roman += romanToDecimal.roman;
+                decimal -= romanToDecimal.decimal;
+            }
+        }
+        return roman;
+    }
+
+    enum RomanToDecimal {
+        TEN("X", 10),
+        FIVE("V", 5),
+        ONE("I", 1);
+
+        private final String roman;
+        private final int decimal;
+
+        RomanToDecimal(String roman, int decimal) {
+            this.roman = roman;
+            this.decimal = decimal;
+        }
+
     }
 }
--- exercise1/src/test/java/pl/nkoder/craftingcodeworkshop/exercise1/RomanNumeralConverterShould.java
+++ exercise1/src/test/java/pl/nkoder/craftingcodeworkshop/exercise1/RomanNumeralConverterShould.java
@@ -9,6 +9,14 @@ public class RomanNumeralConverterShould {
     @Test
     public void generate_a_roman_numeral_for_a_given_decimal() {
         assertThat(romanFor(1)).isEqualTo("I");
+        assertThat(romanFor(2)).isEqualTo("II");
+        assertThat(romanFor(3)).isEqualTo("III");
+        assertThat(romanFor(5)).isEqualTo("V");
+        assertThat(romanFor(7)).isEqualTo("VII");
+        assertThat(romanFor(10)).isEqualTo("X");
+        assertThat(romanFor(17)).isEqualTo("XVII");
+        assertThat(romanFor(30)).isEqualTo("XXX");
+        assertThat(romanFor(38)).isEqualTo("XXXVIII");
     }
 
     private String romanFor(int decimal) {
