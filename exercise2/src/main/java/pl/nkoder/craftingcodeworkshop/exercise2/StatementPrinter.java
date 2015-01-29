679369e8fdb79d1e6a911f8670661762ab661d41
Pawel Barszcz <pawelbarszcz@gmail.com>
2015-01-30 00:41:20 +0100

    Exercise 2 - Account: implemented full feature of printing statement.

--- exercise2/src/main/java/pl/nkoder/craftingcodeworkshop/exercise2/StatementPrinter.java
+++ exercise2/src/main/java/pl/nkoder/craftingcodeworkshop/exercise2/StatementPrinter.java
@@ -1,7 +1,10 @@
 package pl.nkoder.craftingcodeworkshop.exercise2;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Lists.reverse;
 import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class StatementPrinter {
@@ -15,10 +18,12 @@ public class StatementPrinter {
     public void printStatementFor(Transactions transactions) {
         console.writeLine("DATE | AMOUNT | BALANCE");
         int totalAmount = 0;
-        for (Transaction transaction : transactions.orderedFromNewestToOldest()) {
+        List<String> lines = newArrayList();
+        for (Transaction transaction : reverse(transactions.orderedFromNewestToOldest())) {
             totalAmount += transaction.amount();
-            console.writeLine(formatted(transaction, totalAmount));
+            lines.add(formatted(transaction, totalAmount));
         }
+        reverse(lines).forEach(console::writeLine);
     }
 
     private String formatted(Transaction transaction, int totalAmount) {
--- exercise2/src/test/java/pl/nkoder/craftingcodeworkshop/exercise2/StatementPrinterShould.java
+++ exercise2/src/test/java/pl/nkoder/craftingcodeworkshop/exercise2/StatementPrinterShould.java
@@ -46,15 +46,15 @@ public class StatementPrinterShould {
         statementPrinter = new StatementPrinter(console);
 
         statementPrinter.printStatementFor(transactions(
-                transaction(100, dateOf(2014, 1, 28)),
+                transaction(150, dateOf(2014, 1, 30)),
                 transaction(-50, dateOf(2014, 1, 29)),
-                transaction(150, dateOf(2014, 1, 30))));
+                transaction(100, dateOf(2014, 1, 28))));
 
         InOrder inOrder = inOrder(console);
         inOrder.verify(console).writeLine("DATE | AMOUNT | BALANCE");
-        inOrder.verify(console).writeLine("28/01/2014 | 100.00 | 100.00");
-        inOrder.verify(console).writeLine("29/01/2014 | -50.00 | 50.00");
         inOrder.verify(console).writeLine("30/01/2014 | 150.00 | 200.00");
+        inOrder.verify(console).writeLine("29/01/2014 | -50.00 | 50.00");
+        inOrder.verify(console).writeLine("28/01/2014 | 100.00 | 100.00");
         inOrder.verifyNoMoreInteractions();
     }
 
@@ -64,13 +64,13 @@ public class StatementPrinterShould {
         statementPrinter = new StatementPrinter(console);
 
         statementPrinter.printStatementFor(transactions(
-                transaction(0, dateOf(2014, 12, 31)),
-                transaction(0, dateOf(2015, 1, 1))));
+                transaction(0, dateOf(2015, 1, 1)),
+                transaction(0, dateOf(2014, 12, 31))));
 
         InOrder inOrder = inOrder(console);
         inOrder.verify(console).writeLine("DATE | AMOUNT | BALANCE");
-        inOrder.verify(console).writeLine("31/12/2014 | 0.00 | 0.00");
         inOrder.verify(console).writeLine("01/01/2015 | 0.00 | 0.00");
+        inOrder.verify(console).writeLine("31/12/2014 | 0.00 | 0.00");
         inOrder.verifyNoMoreInteractions();
     }
 
