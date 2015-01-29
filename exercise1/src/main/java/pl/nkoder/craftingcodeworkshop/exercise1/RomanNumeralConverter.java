e7c39894d3a6dfbcd22670d8d380d0a94ec548e9
Pawel Barszcz <pawelbarszcz@gmail.com>
2015-01-29 11:55:37 +0100

    Exercise 1 - Roman Numerals: converting all numbers from 1 up to 3999

--- exercise1/src/main/java/pl/nkoder/craftingcodeworkshop/exercise1/RomanNumeralConverter.java
+++ exercise1/src/main/java/pl/nkoder/craftingcodeworkshop/exercise1/RomanNumeralConverter.java
@@ -14,8 +14,18 @@ public class RomanNumeralConverter {
     }
 
     enum RomanToDecimal {
+        THOUSAND("M", 1000),
+        NINE_HUNDREDS("CM", 900),
+        FIVE_HUNDREDS("D", 500),
+        FOUR_HUNDREDS("CD", 400),
+        HUNDRED("C", 100),
+        NINETY("XC", 90),
+        FIFTY("L", 50),
+        FORTY("XL", 40),
         TEN("X", 10),
+        NINE("IX", 9),
         FIVE("V", 5),
+        FOUR("IV", 4),
         ONE("I", 1);
 
         private final String roman;
--- exercise1/src/test/java/pl/nkoder/craftingcodeworkshop/exercise1/RomanNumeralConverterShould.java
+++ exercise1/src/test/java/pl/nkoder/craftingcodeworkshop/exercise1/RomanNumeralConverterShould.java
@@ -11,12 +11,24 @@ public class RomanNumeralConverterShould {
         assertThat(romanFor(1)).isEqualTo("I");
         assertThat(romanFor(2)).isEqualTo("II");
         assertThat(romanFor(3)).isEqualTo("III");
+        assertThat(romanFor(4)).isEqualTo("IV");
         assertThat(romanFor(5)).isEqualTo("V");
         assertThat(romanFor(7)).isEqualTo("VII");
+        assertThat(romanFor(9)).isEqualTo("IX");
         assertThat(romanFor(10)).isEqualTo("X");
         assertThat(romanFor(17)).isEqualTo("XVII");
         assertThat(romanFor(30)).isEqualTo("XXX");
         assertThat(romanFor(38)).isEqualTo("XXXVIII");
+        assertThat(romanFor(40)).isEqualTo("XL");
+        assertThat(romanFor(90)).isEqualTo("XC");
+        assertThat(romanFor(50)).isEqualTo("L");
+        assertThat(romanFor(90)).isEqualTo("XC");
+        assertThat(romanFor(100)).isEqualTo("C");
+        assertThat(romanFor(400)).isEqualTo("CD");
+        assertThat(romanFor(500)).isEqualTo("D");
+        assertThat(romanFor(900)).isEqualTo("CM");
+        assertThat(romanFor(1000)).isEqualTo("M");
+        assertThat(romanFor(3999)).isEqualTo("MMMCMXCIX");
     }
 
     private String romanFor(int decimal) {
